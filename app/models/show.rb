37a1030e7519bdbae9bab2607276d0941467c190
JDM Ubuntu VB <JKDMarks@gmail.com>
2019-04-14 19:36:09 -0400

    Done.

--- app/models/show.rb
+++ app/models/show.rb
@@ -1,3 +1,11 @@
 class Show < ActiveRecord::Base
-  
+  has_many :characters
+  has_many :actors, through: :characters
+  belongs_to :network
+
+  def build_network(call_letters:)
+    network = Network.find_or_create_by(call_letters: call_letters)
+    network.shows << self
+    network
+  end
 end
